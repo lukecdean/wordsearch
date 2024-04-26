0e3e6c6a944080d373a5b6e7d06971705c37289d
lukecdean <ldean35@gatech.edu>
2024-04-25 23:29:23 -0400

    using bigger word bank and writing out to a file

--- findwords.go
+++ findwords.go
@@ -15,6 +15,12 @@ func main() {
     } // if err
     defer file.Close()
 
+    outputfile, err := os.Create("hexwords.txt")
+    if err != nil {
+        fmt.Println("err creating file:", err)
+    } // if err
+    defer outputfile.Close()
+
     scanner := bufio.NewScanner(file)
     var words []string
     //for i := 0; i < wordcount; i++ {
@@ -25,13 +31,17 @@ func main() {
         words = append(words, scanner.Text())
     } // for 
 
-    all(words)
+    all(words, outputfile)
 } // main()
 
-func all(words []string) {
+func all(words []string, outputfile *os.File) {
     for _, word := range words {
         if canBeRepresentedInHex(word) {
-            fmt.Println(word)
+            //fmt.Println(word)
+            _, err := outputfile.WriteString(word + "\n")
+            if err != nil {
+                fmt.Println("err writing to file:", err)
+            } // err
         } // if
     } // for
 } // all()
